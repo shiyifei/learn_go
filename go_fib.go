a13683eb25e341ac6722ae6dc4fd76f9b29d3e1a
shiyifei <shiyifei@xin.com>
2019-09-09 19:04:09 +0800

    use select

--- go_fib.go
+++ go_fib.go
@@ -6,9 +6,9 @@ func main() {
 }
 
 func fib(n int64) int64 {
-	if(n<2) {
+	if n<2 {
 		return n
 	}
-	return fib(n-2)+fib(n-1)
+	return fib(n-2) + fib(n-1)
 }
 
---
+++ use_select.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			//x, y = y, x+y
+
+			temp := x
+			x = y
+			y = temp + y
+
+			case <-quit:
+				fmt.Println("quit")
+				return
+			/*default:
+			fmt.Println("no value received")*/
+		}
+	}
+}
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+
+	defer fibonacci(c, quit)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+}
