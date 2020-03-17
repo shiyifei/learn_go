4a039e9f18ab79aaf7ebb95259a837466372413b
shiyifei <shi_yi_fei@163.com>
2020-03-17 18:22:17 +0800

    merge files

---
+++ use_setuid.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+)
+
+func main() {
+	file, err := os.Open("my_shadow.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data,err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("my_shadow:%s \n", data)
+
+	//打印Uid相关信息
+	printUser("smbuser")
+}
+
+func printUser(username string) {
+	fmt.Printf("current user is:")
+	fmt.Println(user.Current())
+	fmt.Printf("look up user:%s", username)
+	fmt.Println( user.Lookup(username))
+	fmt.Printf("look up userid:0")
+	fmt.Println(user.LookupId("0"))
+}
