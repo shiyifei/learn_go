746e79347b44eb4fd7925fb5e5fc9aa6921df9a0
shiyifei <shiyifei@xin.com>
2020-03-18 10:31:14 +0800

    use_xorm

--- src/use_mysql/use_mysql.go
+++ src/use_mysql/use_mysql.go
@@ -1,33 +1,15 @@
-/**
-	golang操作数据库的实例，包括增删改查
- */
 package main
 
-import (
-	"fmt"
-	"use_mysql/practice"
+import(
+	UseMysql "use_mysql/practice"
 )
 
 func main() {
-	insertId, err := practice.Insert("lixiaoyun", "lixiaoyun@music.com")
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(insertId)
-
-	num, err := practice.Update(200036)
-	if err != nil {
-		fmt.Println(err)
-
-	}
-	fmt.Println(num)
-
-	num, err = practice.Delete(200034)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(num)
-
-	output := practice.ReadData()
-	fmt.Println(output)
+	dbAccess := UseMysql.ConnectToDB()
+	dbAccess.InsertOne()
+	dbAccess.SelectMultiUser()
+	dbAccess.SelectOneUser(2001)
+	dbAccess.UpdateRecord(2001)
+	dbAccess.DeleteRecord(2001)
+	dbAccess.ExecTrans()
 }
