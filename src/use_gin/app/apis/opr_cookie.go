2f058e662b0618d3eb8018577c97015b1fe027ab
shiyifei <shiyifei@xin.com>
2020-04-29 18:24:16 +0800

    use xorm

---
+++ src/use_gin/app/apis/opr_cookie.go
@@ -0,0 +1,23 @@
+package apis
+
+/**
+ * 演示如何操作cookie,注意这里的域名要跟访问地址一致。本机IP：192.168.56.107
+ */
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func ReadCookie(c *gin.Context) {
+	val, _ := c.Cookie("name")
+	c.String(http.StatusOK, "Cookie:%s", val)
+}
+
+func WriteCookie(c *gin.Context) {
+	c.SetCookie("name", "shiyifei", 10, "/", "192.168.56.107", false, true)
+}
+
+func ClearCookie(c *gin.Context) {
+	c.SetCookie("name", "shiyifei", -1, "/", "192.168.56.107", false, true)
+}
--- src/use_xorm/go.mod
+++ src/use_xorm/go.mod
@@ -7,5 +7,6 @@ require (
 	github.com/go-xorm/cmd v0.0.0-20190426080617-f87981e709a1 // indirect
 	github.com/go-xorm/cmd/xorm v0.0.0-20190426080617-f87981e709a1 // indirect
 	github.com/go-xorm/xorm v0.7.9
+	github.com/gogf/gf v1.12.2
 	xorm.io/cmd/xorm v0.0.0-20191108140657-006dbf24bb9b // indirect
 )
